67a1fa6e00c953b42352740a457202febc76594b
Füchs <68223337+Fuechs@users.noreply.github.com>
2022-12-03 14:10:55 +0100

    fix

--- src/memory.py
+++ src/memory.py
@@ -1,34 +1,15 @@
 from inst import Instruction
-# from functools import wraps
-
-# optimize recursive call idk
-# def memoize(func):
-#     cache = {}
-    
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         key = str(args) + str(kwargs)
-        
-#         if key not in cache:
-#             cache[key] = func(*args, **kwargs)
-            
-#         return cache[key]
-    
-#     return wrapper
 
 class Memory:
     def __init__(self, size: int = 1024) -> None:
         self.stack: list[int] = []
-        # self.ram: list[int|None] = [None for i in range(size)]
         # unused for now
+        # self.ram: list[int|None] = [None for i in range(size)]
         self.rom: list[Instruction|int|None] = [None for i in range(size)] 
 
-    def writeROM(self, prog: list) -> None: # inefficient as fuck
-        for stmt in prog:
-            for pos, i in enumerate(self.rom):
-                if i is None:
-                    self.rom[pos] = stmt
-                    break
+    def writeROM(self, prog: list) -> None:
+        for i, stmt in enumerate(prog):
+            self.rom[i] = stmt
 
     def debugROM(self) -> None:
         print("ROM:", end="")
